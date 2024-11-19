b5297463478ad301ffbff746c92d03dfd3109298
amnartaud <amnartaud@gosoft.co.th>
2024-11-19 11:03:02 +0700

    db server

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 /** @format */
 
 const dbConfig = {
-	db: "mongodb+srv://amnart22:Loveyou01235*@amnart.9oc94.mongodb.net/smart-cattle",
+	db: "mongodb://127.0.0.1:27017/smart-cattle",
 };
 
 export default dbConfig;
